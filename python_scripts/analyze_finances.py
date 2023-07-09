f4bca7819a7444d089d4cc173081df022ffffc0e
manumalotra <malotra@ualberta.ca>
2023-07-09 13:38:28 -0600

    Refactoring analyze_finances.py
    Refactored and cleaned up "analyze_finances.py", got rid of functions that I do not use.

--- python_scripts/analyze_finances.py
+++ python_scripts/analyze_finances.py
@@ -10,10 +10,10 @@ pd.set_option('display.max_columns', None)
 #   Read CSV files containing income and expense data    #
 ##########################################################
 
-# Read income data from CSV
+# Read income data from CSV and create income dataframe.
 income_df = pd.read_csv('data_files/income.csv')
 
-# Read expenses data from CSV
+# Read expenses data from CSV and create expenses dataframe.
 expenses_df = pd.read_csv('data_files/expenses.csv')
 
 
@@ -23,30 +23,12 @@ expenses_df = pd.read_csv('data_files/expenses.csv')
 
 income_grouped = af.group_data(income_df)
 
-# print(income_grouped)
-
 
 ##########################################################
 #      Group expenses by year and month (yyyy-mm)        #
 ##########################################################
 
 expenses_grouped = af.group_data(expenses_df)
-
-# print(expenses_grouped)
-
-
-##########################################################
-#                   Basic accounting                     #
-##########################################################
-
-total_income, total_expenses, total_net_income = af.total_income_and_expenses(income_df, expenses_df)
-
-# print(
-# f"""
-# Total income: {total_income}
-# Total expenses: {total_expenses}
-# Total net income: {total_net_income}
-# """)
 
 
 ##########################################################
@@ -78,21 +60,7 @@ af.plot_net_income(income_grouped, expenses_grouped)
 
 
 ##########################################################
-#          Plot all expenses in descending order         #
-##########################################################
-
-# af.plot_all_expenses(expenses_df)
-
-
-#################################################################
-#   Categorized expenses by year-month (yyyy-mm) pivot table    #
-#################################################################
-
-# af.categorized_expenses_year_month(expenses_df)
-
-
-##########################################################
 #    Plot categorized expenses by year-month (yyyy-mm)   #
 ##########################################################
 
-# af.plot_categorized_expenses(expenses_df)
+af.plot_categorized_expenses(expenses_df)
